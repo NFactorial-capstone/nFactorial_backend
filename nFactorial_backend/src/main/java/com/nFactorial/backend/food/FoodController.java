08c0c058b4f90d975c499b8fd4cde70ca2865dac
Minjae <byStander@host.docker.internal>
2024-05-12 15:56:24 +0900

    added FoodVo

--- nFactorial_backend/src/main/java/com/nFactorial/backend/food/FoodController.java
+++ nFactorial_backend/src/main/java/com/nFactorial/backend/food/FoodController.java
@@ -1,5 +1,16 @@
 package com.nFactorial.backend.food;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class FoodController {
 
+	
+	@RequestMapping(value = "/food/search", method = RequestMethod.GET)
+	public String searchFood() {
+		
+		return "";
+	}
 }
---
+++ nFactorial_backend/src/main/java/com/nFactorial/backend/food/FoodVo.java
@@ -0,0 +1,43 @@
+package com.nFactorial.backend.food;
+
+public class FoodVo {
+
+	String foodName;
+	int kcal;
+	int carbo;
+	int protein;
+	int fat;
+	
+	public String getFoodName() {
+		return foodName;
+	}
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+	public int getKcal() {
+		return kcal;
+	}
+	public void setKcal(int kcal) {
+		this.kcal = kcal;
+	}
+	public int getCarbo() {
+		return carbo;
+	}
+	public void setCarbo(int carbo) {
+		this.carbo = carbo;
+	}
+	public int getProtein() {
+		return protein;
+	}
+	public void setProtein(int protein) {
+		this.protein = protein;
+	}
+	public int getFat() {
+		return fat;
+	}
+	public void setFat(int fat) {
+		this.fat = fat;
+	}
+	
+	
+}
