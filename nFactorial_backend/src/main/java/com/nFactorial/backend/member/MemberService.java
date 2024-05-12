812e05f78d1b99aad02c3e83d4b3ac8e4440e598
조성빈 <sxpaper@naver.com>
2024-05-12 20:44:27 +0900

    login-test

--- nFactorial_backend/src/main/java/com/nFactorial/backend/member/MemberService.java
+++ nFactorial_backend/src/main/java/com/nFactorial/backend/member/MemberService.java
@@ -36,28 +36,26 @@ public class MemberService {
 			URL url = new URL(reqURL);
 
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			// POST 요청을 위해 기본값이 false인 setDoOutput을 true로
 
 			conn.setRequestMethod("POST");
 			conn.setDoOutput(true);
-			// POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+			
 
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
 			StringBuilder sb = new StringBuilder();
 			sb.append("grant_type=authorization_code");
 
-			sb.append("&client_id=4b014e54e818307875e7f719754a5313"); // 본인이 발급받은 key
-			sb.append("&redirect_uri=http://localhost:8080/backend/login"); // 본인이 설정한 주소
+			sb.append("&client_id=4b014e54e818307875e7f719754a5313"); 
+			sb.append("&redirect_uri=http://localhost:8080/backend/login"); 
 
 			sb.append("&code=" + authorize_code);
 			bw.write(sb.toString());
 			bw.flush();
 
-			// 결과 코드가 200이라면 성공
 			int responseCode = conn.getResponseCode();
 			System.out.println("responseCode : " + responseCode);
 
-			// 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+			
 			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String line = "";
 			String result = "";
@@ -67,7 +65,7 @@ public class MemberService {
 			}
 			System.out.println("response body : " + result);
 
-			// Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+		
 			JsonParser parser = new JsonParser();
 			JsonElement element = parser.parse(result);
 
@@ -87,7 +85,7 @@ public class MemberService {
 
 	public HashMap<String, Object> getUserInfo(String access_Token) {
 
-		// 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+		
 		HashMap<String, Object> userInfo = new HashMap<String, Object>();
 		String reqURL = "https://kapi.kakao.com/v2/user/me";
 		try {
@@ -95,7 +93,6 @@ public class MemberService {
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 
-			// 요청에 필요한 Header에 포함될 내용
 			conn.setRequestProperty("Authorization", "Bearer " + access_Token);
 
 			int responseCode = conn.getResponseCode();
