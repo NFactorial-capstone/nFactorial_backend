7726113a48fab19902141748da2b791970780e3c
Minjae <mjjung9999@gmail.com>
2024-05-25 18:59:24 +0900

    add VideoUploadController

--- nFactorial_backend/src/main/java/com/nFactorial/backend/uploadfile/ImageUploadController.java
+++ nFactorial_backend/src/main/java/com/nFactorial/backend/uploadfile/ImageUploadController.java
@@ -3,7 +3,6 @@ package com.nFactorial.backend.uploadfile;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.stereotype.Controller;
@@ -12,10 +11,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Base64;
@@ -57,7 +52,7 @@ public class ImageUploadController {
             messageConverters.add(new MappingJacksonHttpMessageConverter());
             restTemplate.setMessageConverters(messageConverters);
 
-            ResponseEntity<Integer> response = restTemplate.postForEntity(FLASK_SERVER_URL, requestEntity, Integer.class);
+            restTemplate.postForEntity(FLASK_SERVER_URL, requestEntity, Integer.class);
 
             System.out.println("done!");
         
